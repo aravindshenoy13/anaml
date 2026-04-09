5dfbfa059288fe2aa5cf7421e1af8425d43c244c
Aravind <aravindshenoy13@gmail.com>
2026-04-09 23:52:56 +0530

    feat(main): Added lifespan event and included all routers

--- main.py
+++ main.py
@@ -3,5 +3,15 @@ from routers.health import health_router
 from routers.inference import inference_router
 from routers.models import model_router
 from core.database import engine, DBBase
+from contextlib import asynccontextmanager
 
-AnaML = FastAPI()
+@asynccontextmanager
+async def run(app):
+    async with engine.begin() as conn:
+        await conn.run_sync(DBBase.metadata.create_all)
+    yield
+
+AnaML = FastAPI(lifespan=run)
+AnaML.include_router(health_router)
+AnaML.include_router(inference_router)
+AnaML.include_router(model_router)
