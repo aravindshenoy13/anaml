540d77b4738da0c0f44c2ddb9bd981921fb8b6d1
Aravind <aravindshenoy13@gmail.com>
2026-04-09 23:38:41 +0530

    feat(main): added db imports

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ from fastapi import FastAPI
 from routers.health import health_router
 from routers.inference import inference_router
 from routers.models import model_router
+from core.database import engine, DBBase
 
 AnaML = FastAPI()
