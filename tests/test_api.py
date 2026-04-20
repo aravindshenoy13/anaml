aa6d90164505ffb479c454992165e2499aef1ce9
Aravind <aravindshenoy13@gmail.com>
2026-04-21 02:31:41 +0530

    test(onnx test): added onnx test

--- tests/test_api.py
+++ tests/test_api.py
@@ -77,12 +77,48 @@ async def test_joblib_workflow(client):
     #predict_model fixture
     payload= {
     "input_data":{
-    "features": [5.1, 3.5, 1.4, 0.2]
+    "features": [[5.1, 3.5, 1.4, 0.2]]
     }}
     predict_response = await client.post(f"/models/{id}/predict", json=payload)
     assert predict_response.status_code == 200, "Model prediction failed"
     data = predict_response.json()
     assert data["output_data"]['predictions'], "Prediction output values not returned"
+
+    #delete_model fixture
+    delete_response = await client.delete(f"/models/{id}")
+    assert delete_response.status_code == 204, "Model deletion failed"
+    
+    #verifying delete
+    verify_response = await client.get(f"/models/{id}")
+    assert verify_response.status_code == 404, "Model exists after deletion"
+
+
+@pytest.mark.asyncio
+async def test_onnx_workflow(client):
+    #create_model fixture
+    data_dict, file_dict = onnx_test_data()
+    create_response = await client.post("/models/register", data = data_dict, files=file_dict)
+    assert create_response.status_code == 201, "Model creation failed"
+    data = create_response.json()
+    id = data["id"]
+    assert data["id"], "Model creation id not returned"
+
+    #get_model fixture
+    get_response = await client.get(f"/models/{id}")
+    assert get_response.status_code == 200, "Model retreival failed"
+    data = get_response.json()
+    assert data["id"] == id, "Model retreival id not matching/not returned"
+
+    #predict_model fixture
+    #FIX FOR ONNX
+    payload= {
+    "input_data":{
+    "X": [[5.1, 3.5, 1.4, 0.2]]
+    }}
+    predict_response = await client.post(f"/models/{id}/predict", json=payload)
+    assert predict_response.status_code == 200, "Model prediction failed"
+    data = predict_response.json()
+    assert data["output_data"], "Prediction output values not returned"
 
     #delete_model fixture
     delete_response = await client.delete(f"/models/{id}")
