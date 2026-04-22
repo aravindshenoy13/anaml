9d5e3dbeafb831cc812e92f2469746bf91af49e7
Aravind <aravindshenoy13@gmail.com>
2026-04-22 14:48:48 +0530

    feat(db model): added metadata column to database

--- models/models.py
+++ models/models.py
@@ -16,6 +16,7 @@ class MLModel(DBBase):
     name = Column(String(255), nullable=False)
     version = Column(String(50), nullable=False)
     description = Column(Text)
+    metadata = Column(Text, nullable=False)
     accuracy = Column(Float)
     weights_path = Column(String(255), nullable=False)
     backend_type = Column(String(50), nullable=False)
