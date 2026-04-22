a5e1946001eb484175797894f2e55e6eed9c34fe
Aravind <aravindshenoy13@gmail.com>
2026-04-22 23:09:53 +0530

    debug

--- tests/test_api.py
+++ tests/test_api.py
@@ -65,32 +65,32 @@ async def test_joblib_workflow(client):
     create_response = await client.post("/models/register", data = data_dict, files=file_dict)
     data = create_response.json()
     assert create_response.status_code == 201, "Model creation failed: " + data["detail"]
-    id = data["id"]
+    model_id = data["id"]
     assert data["id"], "Model creation id not returned"
 
     #get_model fixture
-    get_response = await client.get(f"/models/{id}")
+    get_response = await client.get(f"/models/{model_id}")
     data = get_response.json()
     assert get_response.status_code == 200, "Model retreival failed: " + data["detail"]
-    assert data["id"] == id, "Model retreival id not matching/not returned"
-    assert data["model_metadata"], "Model metadata not returned"
+    assert data["id"] == model_id, "Model retreival id not matching/not returned"
+    assert data["model_metadata"], "Model metadata not returned: " + str(type(data["model_metadata"]))
 
     #predict_model fixture
     payload= {
     "input_data":{
     "features": [[5.1, 3.5, 1.4, 0.2]]
     }}
-    predict_response = await client.post(f"/models/{id}/predict", json=payload)
+    predict_response = await client.post(f"/models/{model_id}/predict", json=payload)
     assert predict_response.status_code == 200, "Model prediction failed"
     data = predict_response.json()
     assert data["output_data"]['predictions'], "Prediction output values not returned: " + data["detail"]
 
     #delete_model fixture
-    delete_response = await client.delete(f"/models/{id}")
-    assert delete_response.status_code == 204, "Model deletion failed: " + delete_response.json()["detail"]
+    delete_response = await client.delete(f"/models/{model_id}")
+    assert delete_response.status_code == 204, "Model deletion failed"
     
     #verifying delete
-    verify_response = await client.get(f"/models/{id}")
+    verify_response = await client.get(f"/models/{model_id}")
     assert verify_response.status_code == 404, "Model exists after deletion" 
 
 
@@ -101,14 +101,14 @@ async def test_onnx_workflow(client):
     create_response = await client.post("/models/register", data = data_dict, files=file_dict)
     data = create_response.json()
     assert create_response.status_code == 201, "Model creation failed: " + data["detail"]
-    id = data["id"]
+    model_id = data["id"]
     assert data["id"], "Model creation id not returned"
 
     #get_model fixture
-    get_response = await client.get(f"/models/{id}")
+    get_response = await client.get(f"/models/{model_id}")
     data = get_response.json()
     assert get_response.status_code == 200, "Model retreival failed: " + data["detail"]
-    assert data["id"] == id, "Model retreival id not matching/not returned"
+    assert data["id"] == model_id, "Model retreival id not matching/not returned"
     assert data["model_metadata"], "Model metadata not returned"
 
     #predict_model fixture
@@ -116,16 +116,16 @@ async def test_onnx_workflow(client):
     "input_data":{
     "X": [[5.1, 3.5, 1.4, 0.2]]
     }}
-    predict_response = await client.post(f"/models/{id}/predict", json=payload)
+    predict_response = await client.post(f"/models/{model_id}/predict", json=payload)
     assert predict_response.status_code == 200, "Model prediction failed"
     data = predict_response.json()
     assert data["output_data"]['predictions'], "Prediction output values not returned: " + data["detail"]
 
     #delete_model fixture
-    delete_response = await client.delete(f"/models/{id}")
-    assert delete_response.status_code == 204, "Model deletion failed: " + delete_response.json()["detail"]
+    delete_response = await client.delete(f"/models/{model_id}")
+    assert delete_response.status_code == 204, "Model deletion failed"
     
     #verifying delete
-    verify_response = await client.get(f"/models/{id}")
+    verify_response = await client.get(f"/models/{model_id}")
     assert verify_response.status_code == 404, "Model exists after deletion"
 
