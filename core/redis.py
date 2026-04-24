102757e2f3fd25ce66f03077df9c0495c2346ed8
Aravind <aravindshenoy13@gmail.com>
2026-04-24 10:57:25 +0530

    feat(redis): added decode_responses so it doesnt save in bytes

--- core/redis.py
+++ core/redis.py
@@ -2,7 +2,7 @@ import redis
 
 from core.config import REDIS_URL
 
-redis_client = redis.asyncio.from_url(REDIS_URL)
+redis_client = redis.asyncio.from_url(REDIS_URL, decode_responses=True)
 
 async def close_redis():
     await redis_client.close()
