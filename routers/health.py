70c08ef7c9e56d9badd2f206b140a21932c5b7a8
Aravind <aravindshenoy13@gmail.com>
2026-04-27 16:29:48 +0530

    feat(redis): added healthcheck for redis in /ready

--- routers/health.py
+++ routers/health.py
@@ -3,6 +3,7 @@ from fastapi.responses import JSONResponse
 from sqlalchemy import text
 
 from core.database import get_session
+from core.redis import redis_client
 
 health_router = APIRouter(prefix="/health")
 
@@ -14,6 +15,7 @@ async def health_live():
 async def health_ready(session = Depends(get_session)):
     try:
         await session.execute(text("SELECT 1"))
+        await redis_client.ping()
     except Exception as e:
         return JSONResponse(status_code = 503, content={"status": str(e)})
     return JSONResponse(content= {"status": "ready"})
