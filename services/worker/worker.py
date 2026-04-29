63e09065b2a7fad255747156174dcb2645add89b
Aravind <aravindshenoy13@gmail.com>
2026-04-29 15:26:09 +0530

    feat(worker): added main loop

--- services/worker/worker.py
+++ services/worker/worker.py
@@ -1,5 +1,6 @@
 import json
 import time
+
 import redis
 
 from core.config import REDIS_URL
@@ -8,16 +9,16 @@ from inference.registry import get_model_class
 
 async def startup():
     redis_client = redis.asyncio.from_url(REDIS_URL, decode_responses=True)
-
+    redis_client.xreadgroup()
     try:
         await redis_client.xgroup_create("inference_jobs", "worker_group", id="0", mkstream=True)
     except Exception:
         pass #group already exists
+    return redis_client
 
 async def process_job(redis_client, message):
     message_id, fields = message
     job_id = fields["job_id"]
-    model_id = fields["model_id"]
     input_data = fields["input_data"]
     backend_type = fields["backend_type"]
     weights_path = fields["weights_path"]
@@ -37,6 +38,7 @@ async def process_job(redis_client, message):
         job_status["status"] = "failed"
         job_status["error_message"] = str(e)
         await redis_client.set(f"job:{job_id}", json.dumps(job_status), ex=3600)
+        return None
     
     latency = time.perf_counter() - inference_start
     job_status = json.loads(await redis_client.get(f"job:{job_id}"))
@@ -44,6 +46,22 @@ async def process_job(redis_client, message):
     job_status["latency"] = latency
     job_status["output_data"] = output_data
     await redis_client.set(f"job:{job_id}", json.dumps(job_status), ex=3600)
+    return message_id
+
+async def main():
+    redis_client = startup()
+
+    while(True):
+        stream, message_list = redis_client.xreadgroup("worker_group", "worker-1", block=0)
+
+        for message in message_list:
+            message_id = process_job(redis_client, message)
+            if message_id:
+                redis_client.xack(stream, "worker_group", message_id)
+
+
+
+
 
 
 
