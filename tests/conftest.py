46b1d0d371bccf2e2ad150cee316957c83b33108
Aravind <aravindshenoy13@gmail.com>
2026-05-29 17:49:01 +0530

    refactor(tests): moved test data helpers to conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,63 @@
+from io import BytesIO
+
 import httpx
+import joblib
 import pytest
 import redis
+import skl2onnx
 from httpx import AsyncClient
+from skl2onnx.common.data_types import FloatTensorType
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from services.api.main import app
 from shared.core.database import DBBase, get_session
 from shared.core.redis import get_redis
 
+
+def joblib_test_data():
+    data_dict = {
+        "name":"test-model",
+        "version":"1.0",
+        "backend_type":"joblib",
+        "description":"cicd test model for joblib",
+        "accuracy":"97.79"
+    }
+    buffer = BytesIO()
+    dset = load_iris()
+    model = RandomForestClassifier()
+    model.fit(dset['data'], dset['target'])
+    joblib.dump(model, buffer)
+
+    file_dict = {"file": 
+            ("test.pkl",
+            buffer.getvalue(),
+            "application/octet-stream"
+        )}
+    return data_dict, file_dict
+
+def onnx_test_data():
+    data_dict = {
+        "name":"test-model",
+        "version":"1.0",
+        "backend_type":"onnx",
+        "description":"cicd test model for onnx",
+        "accuracy":"97.79"
+    }
+    dset = load_iris()
+    model = RandomForestClassifier()
+    model.fit(dset['data'], dset['target'])
+    model = skl2onnx.convert_sklearn(model,initial_types=[("X",FloatTensorType([None, 4]))])
+    model_bytes = model.SerializeToString()
+
+    file_dict = {"file": 
+            ("test.onnx",
+            model_bytes,
+            "application/octet-stream"
+        )}
+    
+    return data_dict, file_dict
 
 @pytest.fixture
 async def engine():
