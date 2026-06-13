df6e8cec3f8b120cced944ad9e137c945bfef744
Aravind <aravindshenoy13@gmail.com>
2026-06-13 12:58:32 +0530

    feat(gateway): added reverse proxy to upstream api

--- services/gateway/main.go
+++ services/gateway/main.go
@@ -1,4 +1,36 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
+)
+
 func main() {
+	upstreamRaw := os.Getenv("UPSTREAM_URL")
+	if upstreamRaw == "" {
+		upstreamRaw = "http://localhost:8000"
+	}
+	upstream, err := url.Parse(upstreamRaw)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(upstream)
+
+	http.Handle("/", proxy)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	addr := ":" + port
+
+	log.Printf("gateway listening on %s -> %s", addr, upstreamRaw)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		log.Fatal(err)
+	}
+
 }
