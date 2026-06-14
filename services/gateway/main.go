445492a1d86e231e5f2d552290eb8faca8c9e467
Aravind <aravindshenoy13@gmail.com>
2026-06-15 00:02:17 +0530

    feat(gateway): wired rate limiter into main

--- services/gateway/main.go
+++ services/gateway/main.go
@@ -6,6 +6,9 @@ import (
 	"net/http/httputil"
 	"net/url"
 	"os"
+	"strconv"
+
+	"github.com/redis/go-redis/v9"
 )
 
 func main() {
@@ -18,9 +21,29 @@ func main() {
 		log.Fatal(err)
 	}
 
+	redisURL := os.Getenv("REDIS_URL")
+	if redisURL == "" {
+		redisURL = "localhost:6379"
+	}
+	redisClient := redis.NewClient(&redis.Options{Addr: redisURL})
+
+	rateString := os.Getenv("RATE_LIMIT_RPS")
+	burstString := os.Getenv("RATE_LIMIT_BURST")
+	if rateString == "" {
+		rateString = "10"
+	}
+	if burstString == "" {
+		burstString = "100"
+	}
+	rate, _ := strconv.ParseFloat(rateString, 64)
+	burst, _ := strconv.Atoi(burstString)
+
 	proxy := httputil.NewSingleHostReverseProxy(upstream)
 
-	http.Handle("/", proxy)
+	limiter := NewRateLimiter(redisClient, rate, burst)
+	handler := RateLimitMiddleware(limiter)(proxy)
+
+	http.Handle("/", handler)
 
 	port := os.Getenv("PORT")
 	if port == "" {
