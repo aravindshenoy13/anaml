e1fc44e5bec5d50e68d2e46866bf954768951599
Aravind <aravindshenoy13@gmail.com>
2026-06-18 16:17:35 +0530

    feat(logging): added middleware to write in main

--- services/gateway/logging.go
+++ services/gateway/logging.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"log/slog"
+	"net/http"
+	"time"
+)
 
 type responseWrapper struct {
 	http.ResponseWriter
@@ -10,4 +14,20 @@ type responseWrapper struct {
 func (rw *responseWrapper) WriteHeader(code int) {
 	rw.statusCode = code
 	rw.ResponseWriter.WriteHeader(code)
+}
+
+func LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		wrapped := &responseWrapper{ResponseWriter: w, statusCode: 200}
+		next.ServeHTTP(wrapped, r)
+		slog.Info(
+			"request",
+			"method", r.Method,
+			"path", r.URL.Path,
+			"status", wrapped.statusCode,
+			"duration_ms", time.Since(start).Milliseconds(),
+			"client_ip", clientKey(r),
+		)
+	})
 }
