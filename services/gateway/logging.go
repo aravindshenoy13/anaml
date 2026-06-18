781413d41fa6192332b93ad3a54f4dc8108ba76b
Aravind <aravindshenoy13@gmail.com>
2026-06-18 16:06:34 +0530

    feat(logging): added basic responseWrapper struct

---
+++ services/gateway/logging.go
@@ -0,0 +1,13 @@
+package main
+
+import "net/http"
+
+type responseWrapper struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (rw *responseWrapper) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
+}
